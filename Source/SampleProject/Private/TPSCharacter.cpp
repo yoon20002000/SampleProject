5ae80152c49276dd319cc215abfdf6ba088c87bd
yoon20002000 <yoon20002000@naver.com>
2024-10-28 16:34:13 +0900

    ABP Add

--- Source/SampleProject/Private/TPSCharacter.cpp
+++ Source/SampleProject/Private/TPSCharacter.cpp
@@ -9,23 +9,20 @@
 // Sets default values
 ATPSCharacter::ATPSCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ATPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ATPSCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -35,37 +32,52 @@ void ATPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 
 	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
 	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
 		{
 			Subsystem->AddMappingContext(InputMappingContext, 0);
 		}
 	}
-	
+
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&ATPSCharacter::Move);
-		EnhancedInputComponent->BindAction(LookAction,ETriggerEvent::Triggered,this,&ATPSCharacter::Look);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Look);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
-		EnhancedInputComponent->BindAction(ShotAction,ETriggerEvent::Triggered,this,&ATPSCharacter::Shot);
+		EnhancedInputComponent->BindAction(ShotAction, ETriggerEvent::Started, this, &ATPSCharacter::Shot);
 	}
 }
 
 void ATPSCharacter::Move(const FInputActionValue& InputActionValue)
 {
-	FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
 
-	UE_LOG(LogTemp, Log, TEXT("Move :  %f %f: "), MovementVector.X, MovementVector.Y);
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
 }
 
 void ATPSCharacter::Look(const FInputActionValue& InputActionValue)
 {
-	FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
-	UE_LOG(LogTemp, Log, TEXT("Look :  %f %f: "), LookAxisVector.X, LookAxisVector.Y);
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);	
+	}
 }
 
 void ATPSCharacter::Shot(const FInputActionValue& InputActionValue)
 {
 	UE_LOG(LogTemp, Log, TEXT("Shot!!"));
 }
-
