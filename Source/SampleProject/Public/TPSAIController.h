a928f05efad03aac6ff51b230618d03c822ce6a3
yoon20002000 <yoon20002000@naver.com>
2024-10-29 17:10:30 +0900

    Add default AI assets

---
+++ Source/SampleProject/Public/TPSAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TPSAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API AYTPSAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
