43af9804b970358438e3260ab547b65f7a24d86d
yoon20002000 <yoon20002000@naver.com>
2024-11-06 17:43:43 +0900

    Add Custom Components
    Add Custom Components
    Change Character Components to Custom Components
    Fix Public include paths

--- Source/SampleProject/SampleProject.Build.cs
+++ Source/SampleProject/SampleProject.Build.cs
@@ -10,7 +10,7 @@ public class SampleProject : ModuleRules
 	
 		PublicIncludePaths.AddRange(
 			new string[] {
-				"TPS",
+				"SampleProject/TPS",
 			}
 		);
 		
---
+++ Source/SampleProject/TPS/Components/CustomCameraComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "CustomCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SAMPLEPROJECT_API UCustomCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+};
