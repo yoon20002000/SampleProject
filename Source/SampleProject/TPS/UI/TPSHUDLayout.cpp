c416c700e501eb017d17ba42adf1182cb49b3fd6
yoon20002000 <yoon20002000@naver.com>
2024-11-12 16:36:42 +0900

    Add HUD Laout class

---
+++ Source/SampleProject/TPS/UI/TPSHUDLayout.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/TPSHUDLayout.h"
+
+#include "CommonUIExtensions.h"
+#include "TPSCommonActivatableWidget.h"
+#include "NativeGameplayTags.h"
+#include "UITag.h"
+#include"CommonUI/Public/Input/CommonUIInputTypes.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TPSHUDLayout)
+
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_UI_LAYER_MENU, "UI.Layer.Menu");
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_UI_ACTION_ESCAPE, "UI.Action.Escape");
+
+UTPSHUDLayout::UTPSHUDLayout(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void UTPSHUDLayout::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	RegisterUIActionBinding(FBindUIActionArgs(FUIActionTag::ConvertChecked(TAG_UI_ACTION_ESCAPE),
+	                                          FSimpleDelegate::CreateUObject(this, &ThisClass::HandleEscapeAction)));
+}
+
+void UTPSHUDLayout::HandleEscapeAction() const
+{
+	if (ensure(EscapeMenuClass.IsNull() == false))
+	{
+		UCommonUIExtensions::PushStreamedContentToLayer_ForPlayer(GetOwningLocalPlayer(), TAG_UI_LAYER_MENU,
+		                                                          EscapeMenuClass);
+	}
+}
---
+++ Source/SampleProject/TPS/UI/TPSHUDLayout.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/TPSCommonActivatableWidget.h"
+#include "TPSHUDLayout.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API UTPSHUDLayout : public UTPSCommonActivatableWidget
+{
+	GENERATED_BODY()
+public:
+	UTPSHUDLayout(const FObjectInitializer& ObjectInitializer);
+
+	virtual void NativeOnInitialized() override;
+
+protected:
+	void HandleEscapeAction() const;
+	UPROPERTY(EditDefaultsOnly)
+	TSoftClassPtr<UCommonActivatableWidget> EscapeMenuClass;
+};
