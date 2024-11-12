c3103f566c62ebdddc3641ef81cfdd236b98991e
yoon20002000 <yoon20002000@naver.com>
2024-11-12 12:35:59 +0900

    Add Custom Class

---
+++ Source/SampleProject/TPS/UI/TPSCommonActivatableWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "TPSCommonActivatableWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API UTPSCommonActivatableWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SampleProject/TPS/UI/TPSCommonUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "TPSCommonUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API UTPSCommonUserWidget : public UCommonUserWidget
+{
+	GENERATED_BODY()
+	
+};
