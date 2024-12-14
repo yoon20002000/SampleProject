4bc0a795239e79996234e96f68fc43482f765ec0
yoon20002000 <yoon20002000@naver.com>
2024-12-14 15:35:11 +0900

    기존 Projectile Action을 대체할 Attack 틀 추가

---
+++ Source/SampleProject/TPS/Game/AbilitySystem/TPSGA_Attack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/TPSGameplayAbility.h"
+#include "TPSGA_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API UTPSGA_Attack : public UTPSGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
