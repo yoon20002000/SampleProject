1132c5a35ba7218d2ab456947631f3b68bab7b86
yoon20002000 <yoon20002000@naver.com>
2025-01-17 16:34:12 +0900

    Actor Canvas 및 Layout 작업 진행 중

--- Source/SampleProject/TPS/UI/STPSActorCanvas.h
+++ Source/SampleProject/TPS/UI/STPSActorCanvas.h
@@ -3,6 +3,7 @@
 #pragma once
 
 
+class UTPSIndicatorDescriptor;
 /**
  * 
  */
@@ -12,7 +13,27 @@ public:
 	class FSlot : public TSlotBase<FSlot>
 	{
 	public:
+		FSlot(UTPSIndicatorDescriptor* InIndicator): TSlotBase<FSlot>(),
+		Indicator(InIndicator)
+		{
+			
+		}
 		
-		
+	private:
+
+		UTPSIndicatorDescriptor* Indicator;
+		FVector2D ScreenPosition;
+		double Depth;
+		int32 Priority;
+
+		uint8 bIsIndicatorVisible : 1;
+		uint8 bInFrontOfCamera : 1;
+		uint8 bHasValidScreenPosition :1;
+		uint8 bDirty : 1;
+
+		mutable  uint8 bWasIndicatorClamped :1;
+		mutable  uint8 bWasIndicatorClampedStatusChanged :1;
+
+		friend class SActorCanvas;
 	};
 };
