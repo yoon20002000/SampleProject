4c822c6d83f5ac50852f9ca07a28e85ebdfe0e71
yoon20002000 <yoon20002000@naver.com>
2025-02-17 23:19:35 +0900

    주석 추가

--- Source/SampleProject/TPS/Game/TPSWorldData.h
+++ Source/SampleProject/TPS/Game/TPSWorldData.h
@@ -4,30 +4,36 @@
 
 class ATPSContainer;
 
+// NormalActorData
 USTRUCT(BlueprintType)
 struct FWorldActorData : public FTableRowBase
 {
 	GENERATED_BODY()
+public:
 	FWorldActorData() : Actor(nullptr), ActorTransform(FTransform::Identity)
 	{
 		
 	}
 	
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSoftClassPtr<AActor> Actor;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FTransform ActorTransform;
 };
 
+// ItemComponent에 들어 있을 Item의 Data
 USTRUCT(BlueprintType)
 struct FItemComponentData
 {
 	GENERATED_BODY()
+public:
 	FItemComponentData() : InventorySlotIndex(-1), Item(), Quantity(-1)
 	{
 		
 	}
 	
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 InventorySlotIndex;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
@@ -36,15 +42,18 @@ struct FItemComponentData
 	int32 Quantity;
 };
 
+// ContainerActor의 ItemComponent 설정용 Data
 USTRUCT(BlueprintType)
 struct FContainerActorData : public FTableRowBase
 {
 	GENERATED_BODY()
+public:
 	FContainerActorData() : ContainerActor(nullptr), ActorTransform(FTransform::Identity), InventoryMaxStackSize(1),
 	                        ContainedItems()
 	{
 	}
-
+	
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSoftClassPtr<ATPSContainer> ContainerActor;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
