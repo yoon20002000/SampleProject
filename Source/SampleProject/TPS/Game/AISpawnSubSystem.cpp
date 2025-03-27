6e18b0657590d6591ff618c230648b7fe61b204b
yoon20002000 <yoon20002000@naver.com>
2025-03-27 21:04:59 +0900

    우선적으로 AISpawnSubsystem을 실행하기 위한 기반 작업 적용

---
+++ Source/SampleProject/TPS/Game/AISpawnSubSystem.cpp
@@ -0,0 +1,33 @@
+#include "Game/AISpawnSubSystem.h"
+
+#include "Character/TPSCharacter.h"
+
+bool UAISpawnSubSystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	bool IsMainGameLevel = Outer->GetName().Equals(TEXT("MainGame"));
+	
+	return Super::ShouldCreateSubsystem(Outer) && IsMainGameLevel == true;
+}
+
+void UAISpawnSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UAISpawnSubSystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+}
+
+void UAISpawnSubSystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+ATPSCharacter* UAISpawnSubSystem::SpawnAICharacter(const TSubclassOf<ATPSCharacter>& InSpawnCharacterClass, const FVector& InSpawnLocation,
+                                                   const FRotator& InSpawnRotator, const FActorSpawnParameters& InActorSpawnParameter)
+{
+	ATPSCharacter* SpawnCharacter = GetWorld()->SpawnActor<ATPSCharacter>(InSpawnCharacterClass, InSpawnLocation, InSpawnRotator, InActorSpawnParameter);
+	
+	return SpawnCharacter;
+}
---
+++ Source/SampleProject/TPS/Game/AISpawnSubSystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Subsystems/WorldSubsystem.h"
+#include "AISpawnSubSystem.generated.h"
+
+class ATPSCharacter;
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API UAISpawnSubSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual void Deinitialize() override;
+
+private:
+	ATPSCharacter* SpawnAICharacter(const TSubclassOf<ATPSCharacter>& InSpawnCharacterClass,
+	                                const FVector& InSpawnLocation,
+	                                const FRotator& InSpawnRotator, const FActorSpawnParameters& InActorSpawnParameter);
+};
