0b2a510a40ef0d69f55fdf0b005004158ad8c08e
yoon20002000 <yoon20002000@naver.com>
2025-04-16 08:40:15 +0900

    NonPlayer 추가

---
+++ Source/SampleProject/TPS/Character/TPSNonPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TPSCharacter.h"
+#include "TPSNonPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT_API ATPSNonPlayer : public ATPSCharacter
+{
+	GENERATED_BODY()
+	
+};
